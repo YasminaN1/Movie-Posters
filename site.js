650014475d9d8060f0869f7e3807c541e2f2770d
YasminaN1 <526ynigmanova@frhsd.com>
2025-12-11 02:39:18 +0000

    Create a custom theme/design/style for your site using CSS.

--- site.js
+++ site.js
@@ -45,50 +45,53 @@
 // vue_app.mount("#vue_app")
 
 
-
+/* Vue App Setup */
 const vue_app = Vue.createApp({
+	/* Function that autonaticaly louds movies.json and stores the data */
 	created () {
 		fetch('movies.json')
 			.then(response => response.json())
 			.then(json => {
-				this.movies = json
+				this.movies = json  /* movies: Array of movie objevts */
 			})
 	},
 
 	data() {
 		return {
-			movies: [],
+			movies: [], /* Array: loaded from movies.json */
+			// strings
 			title: "The Retro Cinema Collection",
 			owner: "Yaz's Github",
 			githubLink: "https://github.com/YasminaN1/Movie-Posters"
 		}
 	},
-
+/* Functions: Converts a release date array into text date */
 	methods: {
 		makeTextDate(dateArray) {
 			const [year, month, day] = dateArray
 			return `${year}-${month}-${day}`
 		},
-
+ /* Converts minutes into minutes text */
 		timeText(minutes) {
 			return `${minutes} mins`
 		},
-
+/* Converts minutes into hours text (overwrites the previous timeText) */
 		timeText(minutes) {
 			return `${(minutes/60).toFixed(1)} hrs`
 		},
-
+ /* Incremints like count  input: index (Number) */
 		like(index){
 			this.movies[index].likes++
 		},
-
+/* Increments dislike count */
 		dislike(index){
 			this.movies[index].dislikes++
 		},
-
+/* posterindex cycles withon the posters array lenght */ 
 		posterClick(index){
-			let movie = this.movies[index]
-			movie.posterindex = (movie.posterindex + 1) % movie.posters.length
+			let movie = this.movies[index] /* Object: selected movie */
+			movie.posterindex = (movie.posterindex + 1) % movie.posters.length //Uses modulo (%) to loop through poster images  ( calculates the remeinder left over after dividign one nunber)
+
 		}
 	}
 })
